9a21315a54fdafe28e52745eaa941a176f1ba1ad
student <student@Y5-1>
2022-11-27 17:53:57 +0300

    Добавьте файлы проекта.

---
+++ MVVM/JsonFileServis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    internal class JsonFileService : IFileServis
+    {
+        public List<Phone> Open(string filename)
+        {
+            List<Phone> phones = new List<Phone>();
+            DataContractJsonSerializer jsonFarmatter = new DataContractJsonSerializer(typeof(Phone));
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                phones = jsonFarmatter.ReadObject(fs) as List<Phone>;
+            }
+            return phones;
+        }
+
+        public void Save(string filename, List<Phone> phonesList)
+        {
+            DataContractJsonSerializer jsonFarmatter = new DataContractJsonSerializer(typeof(Phone));
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                jsonFarmatter.WriteObject(fs, phonesList);
+            }
+        }
+    }
+}
